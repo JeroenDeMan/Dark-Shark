d7966f5ede4c43f7a0503c649c3ad1120d5c5dba
Vuong BUI <37221772+vuong-buihv@users.noreply.github.com>
2020-11-18 14:35:00 +0100

    Parking Lot

---
+++ src/main/java/be/darkshark/parkshark/domain/entity/ParkingCategory.java
@@ -0,0 +1,5 @@
+package be.darkshark.parkshark.domain.entity;
+
+public enum ParkingCategory {
+    UNDERGROUND_BUILDING, ABOVE_GROUND_BUILDING
+}
--- src/main/java/be/darkshark/parkshark/domain/entity/ParkingLot.java
+++ src/main/java/be/darkshark/parkshark/domain/entity/ParkingLot.java
@@ -1,4 +1,36 @@
 package be.darkshark.parkshark.domain.entity;
 
+import be.darkshark.parkshark.domain.entity.person.Employee;
+import be.darkshark.parkshark.domain.entity.util.Address;
+
+import javax.persistence.*;
+
+@Entity
 public class ParkingLot {
+    @Id
+    @SequenceGenerator(name = "parkinglot_seq", sequenceName = "parkinglot_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "parkinglot_seq")
+    private long id;
+
+
+    @Column(name = "name")
+    private String name;
+
+    @Enumerated
+    @Column(name = "category")
+    private ParkingCategory parkingCategory;
+
+    @Column(name = "capacity")
+    private int capacity;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "contact_person")
+    private Employee contactPerson;
+
+    @Embedded
+    @Column(name = "address")
+    private Address address;
+
+//    @Column(name = "pricePerHour")
+//    private
 }
