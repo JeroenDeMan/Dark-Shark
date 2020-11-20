86fd6f8d4833bfc1c0298d1d2e8824a356aa7376
Jeroen De Man <jeroendm26@gmail.com>
2020-11-20 10:32:12 +0100

    changed meberservice to transactional

--- src/main/java/be/darkshark/parkshark/service/MemberService.java
+++ src/main/java/be/darkshark/parkshark/service/MemberService.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +18,7 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Service
+@Transactional
 public class MemberService {
 
     private final Logger log = LoggerFactory.getLogger(MemberService.class);
